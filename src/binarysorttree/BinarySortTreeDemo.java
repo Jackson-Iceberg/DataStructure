0e03e58ee071b8c9f8b3d23a99f9238569028909
Jackson <zhouhao010@gmail.com>
2022-05-04 12:58:16 +0100

    BinarySortTree Leaf Node delete function

--- src/binarysorttree/BinarySortTreeDemo.java
+++ src/binarysorttree/BinarySortTreeDemo.java
@@ -2,7 +2,7 @@ package binarysorttree;
 
 public class BinarySortTreeDemo {
 	public static void main(String[] args) {
-		int[] array = {7,3,10,12,5,1,9};
+		int[] array = {7,3,10,12,5,1,9,2};
 		BinarySortTree bst = new BinarySortTree();
 		// 循环的添加结点到二叉排序树
 		for(int i = 0;i<array.length;i++) {
@@ -11,12 +11,63 @@ public class BinarySortTreeDemo {
 		// 中序遍历二叉排序树
 		System.out.println("中序遍历二叉排序树");
 		bst.infixOrder();
+		
+		// 测试一下删除叶子结点
+		bst.deleNode(2);
+		bst.deleNode(5);
+		bst.deleNode(9);
+		bst.deleNode(12);
+		bst.deleNode(1);
+		System.out.println("删除结点后的二叉排序树");
+		bst.infixOrder();
 	}
 }
 
 // 创建二叉排序树
 class BinarySortTree{
 	private Node root;
+	// 查找要删除的结点
+	public Node search(int value) {
+		if(root == null) {
+			return null;
+		}else return root.search(value);
+	}
+	
+	// 查找父结点
+	public Node searchParent(int value) {
+		if(root == null) return null;
+		else return root.searchParent(value);
+	}
+	
+	// 删除结点
+	public void deleNode(int value) {
+		if(root == null) return;
+		else {
+			// 1. 先找到要删除的结点
+			Node targetNode = search(value);
+			// 如果没有找到要删除的结点
+			if(targetNode == null) {
+				return;
+			}
+			// 如果发现这个二叉排序树只有一个结点
+			if(root.left == null && root.right == null) {
+				root = null;
+				return;
+			}
+			// 找targetNode的父节点
+			Node parent = searchParent(value);
+			// 说明是叶子结点，去判断该结点是父节点的左子结点还是右子结点
+			if(targetNode.left == null && targetNode.right == null) {
+				if(parent.left != null && parent.left.value == value) {
+					parent.left = null;
+				}else if(parent.right != null && parent.right.value == value) {
+					parent.right = null;
+				}
+			}
+			
+		}
+	}
+	
 	// 添加结点
 	public void add(Node node) {
 		if(root == null) {
@@ -42,6 +93,47 @@ class Node{
 	Node right;
 	public Node(int value) {
 		this.value = value;
+	}
+	// 查找删除的结点
+	// value 即希望删除的结点的值
+	public Node search(int value) {
+		if(value == this.value) {
+			return this;
+		}
+		else if(value < this.value) {
+			// 如果左子节点为空
+			if(this.left == null) {
+				return null;
+			}else {
+				return this.left.search(value);
+			}
+		}
+		else {
+			if(this.right == null) {
+				return null;
+			}else {
+				return this.right.search(value);
+			}
+		}
+	}
+	// 查找要删除结点的父节点
+	// value是要删除结点的值
+	// 返回要删除结点的父节点，如果没有则返回null
+	public Node searchParent(int value) {
+		// 如果当前结点就是要删除的结点的父节点，就返回
+		if((this.left != null && this.left.value == value)||
+			(this.right != null && this.right.value == value)) {
+			return this;
+		}
+		// 如果查找的值小于当前结点的值，且当前结点的左子结点不为空，往左边去找
+		else {
+			if(value < this.value && this.left != null) {
+				return this.left.searchParent(value);
+			}
+			else if(value > this.value && this.right != null) {
+				return this.right.searchParent(value);
+			}else return null; // 没有找到
+		}
 	}
 	
 	/// 添加结点的方法
